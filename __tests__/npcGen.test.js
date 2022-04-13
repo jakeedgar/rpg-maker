2675b67aa09aa9328a49da9419f637e150112e78
Michael Wright <m.wright92@icloud.com>
2022-04-13 15:34:58 -0700

    add enemyType method and pass test
    Co-authored-by: Jake Edgar <jakeedgar@users.noreply.github.com>
    Co-authored-by: Meron G. Tekie <MeronTekie@users.noreply.github.com>

--- __tests__/npcGen.test.js
+++ __tests__/npcGen.test.js
@@ -2,14 +2,34 @@ import npcClass from '../src/js/npcGen.js';
 
 describe( 'npcClass',()=>{
   let npcPlayer;
+  
   beforeEach(()=>{
   npcPlayer = new npcClass(4,6,7,8);
   })
-  test ("Should return the npc  stats ",()=>{
+
+  test ("Should return the npc stats ",()=>{
 
   expect(npcPlayer.strength).toEqual(4);
   expect(npcPlayer.intelligence).toEqual(6);
   expect(npcPlayer.dexterity).toEqual(7);
   expect(npcPlayer.constitution).toEqual(8);
+  });
+
+  test("should update player stats based on player type input", () => {
+    npcPlayer.enemyType("enemy");
+    expect(npcPlayer.strength).toEqual(8);
+    expect(npcPlayer.intelligence).toEqual(7);
+    expect(npcPlayer.dexterity).toEqual(8);
+    expect(npcPlayer.constitution).toEqual(7);
+    npcPlayer.enemyType("wolf");
+    expect(npcPlayer.strength).toEqual(5);
+    expect(npcPlayer.intelligence).toEqual(6);
+    expect(npcPlayer.dexterity).toEqual(5);
+    expect(npcPlayer.constitution).toEqual(5);
+    npcPlayer.enemyType("rat");
+    expect(npcPlayer.strength).toEqual(4);
+    expect(npcPlayer.intelligence).toEqual(4);
+    expect(npcPlayer.dexterity).toEqual(4);
+    expect(npcPlayer.constitution).toEqual(4)
   });
 });
--- src/js/npcGen.js
+++ src/js/npcGen.js
@@ -6,6 +6,25 @@ export default class npcClass {
     this.constitution = constitution;
     this.type = {};
   }
+  enemyType(npcPlayer) {
+    if (npcPlayer === "enemy") {
+      this.strength = 8;
+      this.intelligence = 7;
+      this.dexterity = 8;
+      this.constitution = 7;
+      this.type = "enemy";
+    } else if(npcPlayer === "wolf") {
+      this.strength = 5;
+      this.intelligence = 6;
+      this.dexterity = 5;
+      this.constitution = 5;
+      this.type = "wolf";
+    }else {
+      this.strength = 4;
+      this.intelligence = 4;
+      this.dexterity = 4;
+      this.constitution = 4;
+      this.type = "rat";
+    }
+  }
 }
-
-
