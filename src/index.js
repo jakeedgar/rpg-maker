d2695c9593fd07052ed92e67ead1b07a16273717
Jake Edgar <jakeedgar1012@gmail.com>
2022-04-14 14:59:27 -0700

    update basic UI functionality
    Co-authored-by: Meron G. Tekie <MeronTekie@users.noreply.github.com>
    Co-authored-by: m-wright92 <m-wright92@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import npcClass from "./js/npcGen";
+import PlayerClass from "./js/playerClass";
+import Battle from './js/battle';
+
+$(document).ready(function() {
+  $("#game").submit(function(event) {
+    event.preventDefault();
+    const playerClass = $("input:radio[name=player]:checked").val();
+    console.log(playerClass);
+    const playerOne = new PlayerClass();
+    playerOne.vocation(playerClass);
+    console.log(playerOne);
+    const enemy = new npcClass();
+    
+
+    const game = new Battle(playerOne, enemy)
+
+    $("#rat-gen").on("click",function() {
+      enemy.enemyType("rat");
+      console.log(enemy);
+    });
+  });
+});
